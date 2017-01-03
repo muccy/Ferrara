b24274f6825a5e1263a4f6a18adf5ff1c4e673b7
Muccy <muccymac@gmail.com>
2017-01-03 11:37:23 +0100

    Tests project

---
+++ Ferrara/Ferrara/Ferrara.h
@@ -0,0 +1,19 @@
+//
+//  Ferrara.h
+//  Ferrara
+//
+//  Created by Marco on 03/01/17.
+//  Copyright © 2017 MeLive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ferrara.
+FOUNDATION_EXPORT double FerraraVersionNumber;
+
+//! Project version string for Ferrara.
+FOUNDATION_EXPORT const unsigned char FerraraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ferrara/PublicHeader.h>
+
+
