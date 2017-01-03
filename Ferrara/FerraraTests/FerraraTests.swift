c0993ebe8d1169ff3d18274a369eadc6270326da
Muccy <muccymac@gmail.com>
2017-01-03 19:11:58 +0100

    First dummy test

--- Ferrara/FerraraTests/FerraraTests.swift
+++ Ferrara/FerraraTests/FerraraTests.swift
@@ -10,27 +10,12 @@ import XCTest
 @testable import Ferrara
 
 class FerraraTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testInit() {
+        let source = ["A"]
+        let destination = ["B"]
+        let diff = Diff(from: source, to: destination)
+        
+        XCTAssert(diff.source == source)
+        XCTAssert(diff.destination == destination)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
